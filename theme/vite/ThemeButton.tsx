7a087bf5e10504b013b1911863292fcdd4dafeac
William Roberts <123806743+williambroberts@users.noreply.github.com>
2023-09-07 08:21:29 +0100

    Update ThemeButton.tsx

--- theme/vite/ThemeButton.tsx
+++ theme/vite/ThemeButton.tsx
@@ -1,1 +1,39 @@
 //
+"use client"
+import {FiSun,FiMoon} from "react-icons/fi"
+import useTheme from "./useTheme"
+type ThemeType = "light"|"dark"
+import styles from "./ThemeButton.module.css"
+import { memo } from "react"
+function getInitialTheme():ThemeType {
+    let initialTheme:any = "light"
+    try{
+        initialTheme = localStorage.getItem('data-theme')
+    }catch(e){
+
+    }
+    if (initialTheme){
+       
+        return initialTheme
+    }
+    //
+    let match = "(prefers-color-scheme: dark)"
+     if (window.matchMedia(match).matches){
+        return "dark"
+     }
+
+
+    return "light"
+}
+ const ThemeButton = () => {
+    const [theme,handleTheme]=useTheme(getInitialTheme)
+  return (
+    <button 
+    aria-label="theme"
+    className={styles.theme}
+    onClick={handleTheme}>
+        {theme==="light"? <FiSun/>:<FiMoon/>}
+    </button>
+  )
+}   
+export default memo(ThemeButton)
